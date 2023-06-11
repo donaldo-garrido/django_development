a749731c828416024d46f3a5068eb7f392f979c8
DonaldoAG99999 <donaldhereicih@hotmail.com>
2023-06-11 15:59:22 -0600

    Update

--- django_essential_training/notes/views.py
+++ django_essential_training/notes/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import DetailView, ListView
 
 from .models import Notes
 # Create your views here.
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = 'notes'
+
+#def list(request):
+#    all_notes = Notes.objects.all() #Stores all the notes
+#    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+
+class NotesDeatilView(DetailView):
+    model = Notes
+    context_object_name = 'note'
+
+def detail(request, pk):
+    
+
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note doesn't exist")
+    return render(request, 'notes/notes_detail.html', {'note':note})
